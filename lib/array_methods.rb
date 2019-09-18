48883616ad9dd072a3c87de3540d42169486fdf1
Barak Saidoff <barak.saidoff@gmail.com>
2019-09-18 19:42:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,16 @@
 def find_element_index(array, value_to_find)
-  i =0
-  while array[i] do
-    
     
     
 end
 
 def find_max_value(array)
   
+  
+  
 end
 
 def find_min_value(array)
+  
+  
   
 end
